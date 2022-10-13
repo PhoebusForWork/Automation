9f0b4f9f02ee894d6fb2cdd3b3ea81f8ed872bf7
phoebus liu <phoebusliu@test.com>
2022-10-13 14:50:32 +0800

    調整assert or 用法,與字串format>-f(string)

--- utils/postgres_util.py
+++ utils/postgres_util.py
@@ -5,27 +5,26 @@ class User_wallet:
     @staticmethod
     def user_wallet_reset(balance="1000", user_id="29"):
         reset = Postgresql(database='wallet', platform="plt")
-        sql = "update wallet.vs_wallet set balance = {} ,freeze_balance=0 where user_id = {} ;".format(
-            balance, user_id)
+        sql = - \
+            f"update wallet.vs_wallet set balance = {balance} ,freeze_balance=0 where user_id = {user_id} ;"
         reset.run_sql(sql=sql)
 
     @staticmethod
     def user_wallet_deposit_check(user_id="29", check_amount=100, channel="AWC"):
         check = Postgresql(database='wallet', platform='plt')
-        game_transfer = "select type,amount from wallet.vs_game_transfer where user_id = {} order by create_time desc limit 1;".format(
-            user_id)
+        game_transfer = - \
+            f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
         data = check.select_sql(game_transfer)
         Type, amount = data[0][0], data[0][1]
-        game_wallet = "select channel_code,balance,freeze_balance from wallet.vs_game_wallet where user_id = {};".format(
-            user_id)
+        game_wallet = - \
+            f"select channel_code,balance,freeze_balance from wallet.vs_game_wallet where user_id = {user_id};"
         data = check.select_sql(game_wallet)
         channel_code, balance, freeze_balance = data[0][0], data[0][1], data[0][2]
-        # print(Type, amount, channel_code, balance, freeze_balance)
         try:
             assert Type == "DEPOSIT"
             assert amount == check_amount
             assert channel_code == "AWC"
-            assert balance or freeze_balance == check_amount
+            assert check_amount in [balance, freeze_balance]
             return True
         except:
             return False
@@ -33,12 +32,12 @@ class User_wallet:
     @staticmethod
     def user_wallet_withdraw_check(user_id="29", check_amount=100, channel="AWC"):
         check = Postgresql(database='wallet', platform='plt')
-        game_transfer = "select type,amount from wallet.vs_game_transfer where user_id = {} order by create_time desc limit 1;".format(
-            user_id)
+        game_transfer = - \
+            f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
         data = check.select_sql(game_transfer)
         Type, amount = data[0][0], data[0][1]
-        game_wallet = "select balance,freeze_balance from wallet.vs_wallet where user_id = {};".format(
-            user_id)
+        game_wallet = - \
+            f"select balance,freeze_balance from wallet.vs_wallet where user_id = {user_id};"
         data = check.select_sql(game_wallet)
         balance, freeze_balance = data[0][0], data[0][1]
         try:
