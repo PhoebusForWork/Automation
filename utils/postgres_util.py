abaeae007d15bca65fd8e54cbd54e1bab4adf3fa
phoebus liu <phoebusliu@test.com>
2022-10-13 15:26:07 +0800

    遺漏一組assert or 用法調整(補上)

--- utils/postgres_util.py
+++ utils/postgres_util.py
@@ -5,19 +5,16 @@ class User_wallet:
     @staticmethod
     def user_wallet_reset(balance="1000", user_id="29"):
         reset = Postgresql(database='wallet', platform="plt")
-        sql = - \
-            f"update wallet.vs_wallet set balance = {balance} ,freeze_balance=0 where user_id = {user_id} ;"
+        sql = f"update wallet.vs_wallet set balance = {balance} ,freeze_balance=0 where user_id = {user_id} ;"
         reset.run_sql(sql=sql)
 
     @staticmethod
     def user_wallet_deposit_check(user_id="29", check_amount=100, channel="AWC"):
         check = Postgresql(database='wallet', platform='plt')
-        game_transfer = - \
-            f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
+        game_transfer = f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
         data = check.select_sql(game_transfer)
         Type, amount = data[0][0], data[0][1]
-        game_wallet = - \
-            f"select channel_code,balance,freeze_balance from wallet.vs_game_wallet where user_id = {user_id};"
+        game_wallet = f"select channel_code,balance,freeze_balance from wallet.vs_game_wallet where user_id = {user_id};"
         data = check.select_sql(game_wallet)
         channel_code, balance, freeze_balance = data[0][0], data[0][1], data[0][2]
         try:
@@ -32,18 +29,16 @@ class User_wallet:
     @staticmethod
     def user_wallet_withdraw_check(user_id="29", check_amount=100, channel="AWC"):
         check = Postgresql(database='wallet', platform='plt')
-        game_transfer = - \
-            f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
+        game_transfer = f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
         data = check.select_sql(game_transfer)
         Type, amount = data[0][0], data[0][1]
-        game_wallet = - \
-            f"select balance,freeze_balance from wallet.vs_wallet where user_id = {user_id};"
+        game_wallet = f"select balance,freeze_balance from wallet.vs_wallet where user_id = {user_id};"
         data = check.select_sql(game_wallet)
         balance, freeze_balance = data[0][0], data[0][1]
         try:
             assert Type == "WITHDRAW"
             assert amount == check_amount
-            assert balance or freeze_balance == check_amount
+            assert check_amount in [balance, freeze_balance]
             return True
         except:
             return False
