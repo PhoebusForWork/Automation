94117babf9f67b241cd970d899a3ca0b41ebc09f
phoebus liu <phoebusliu@test.com>
2022-10-14 15:56:26 +0800

    補上staticmethod

--- utils/generator.py
+++ utils/generator.py
@@ -5,14 +5,17 @@ import string
 
 class Make():
 
+    @staticmethod
     def name(namelen=8):
         return ''.join(random.choice(string.ascii_letters + string.digits)
                        for _ in range(namelen))
 
+    @staticmethod
     def mobile():
         return "1{}{}".format(random.choice('589'), ''.join(
             random.choice(string.digits) for x in range(9)))
 
+    @staticmethod
     def date(status):
         if status == 'start':
             return time.strftime('%Y-%m-%d ', time.localtime()) + '00:00:00'
