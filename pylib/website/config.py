04712359c14cc4a42cb04a4ca2fb93df521f1428
phoebus liu <phoebusliu@test.com>
2022-10-17 12:25:04 +0800

    只是一個前端的api...

---
+++ pylib/website/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import configparser
+from ..website.webApiBase import WEB_API  # 執行RF時使用
+from utils.generator import Make
+
+
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+web_host = config['host']['web_host']
+
+
+class Config(WEB_API):  # 客戶詳細資料
+
+    def get_avatar_urls(self):  # 獲取用戶明細資料
+        response = self.ws.get(web_host+"/v1/config/avatar/urls",
+                               data={},
+                               params={}
+                               )
+        self._printresponse(response)
+        return response.json()
---
+++ testcase/client_side/test_config.py
@@ -0,0 +1,34 @@
+import pytest
+import allure
+from utils.dataUtils import Utils
+from utils.APIController import Cs_API_Controller
+from utils.postgres_util import User_wallet
+from pylib.website.wallet import Wallet
+
+td = Utils()
+testData = td.read_json5('test_config.json5', file_side='cs')
+
+
+######################
+#  setup & teardown  #
+######################
+
+#############
+# test_case #
+#############
+
+
+@allure.feature("基本配置")
+@allure.story("頭像連結列表")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test_case, req_method, req_url, json, scenario, target, params, code_status, keyWord", td.get_test_case(testData, 'get_config_avatar_urls'))
+def test_get_config_avatar_urls(test_case, req_method, req_url, json, scenario, target, params, code_status, keyWord, getCsLoginToken):
+
+    # json_replace = td.replace_json(json, target)
+
+    api = Cs_API_Controller()
+    resp = api.HttpsClient(req_method, req_url, json,
+                           params, token=getCsLoginToken)
+
+    assert resp.status_code == code_status, resp.text
+    assert keyWord in resp.text
