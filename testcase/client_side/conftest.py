48bf7968a6dd254290e0ad4913445b59be2e29b6
phoebus liu <phoebusliu@test.com>
2022-12-22 11:19:34 +0800

    提交全域變數修正,還有拿掉print

--- testcase/client_side/conftest.py
+++ testcase/client_side/conftest.py
@@ -8,6 +8,7 @@ config = configparser.ConfigParser()
 config.read('config/config.ini')
 cs_account = config['cs_account']['account']
 cs_password = config['cs_account']['password']
+login_user_id = None
 
 
 @pytest.fixture(scope="session")
@@ -16,8 +17,7 @@ def getCsLoginToken(username=cs_account, password=cs_password):
     resp = api.Login(username=username, password=password)
     try:
         token = resp.json()['data']['token']
-        global user_id
-        user_id = resp.json()['data']['userId']
+        login_user_id = resp.json()['data']['userId']
     except Exception as ex:
         logging.error('登錄失敗！接口返回:{}'.format(resp.text))
         traceback.print_tb(ex)
@@ -27,8 +27,8 @@ def getCsLoginToken(username=cs_account, password=cs_password):
 
 @pytest.fixture(scope="session")
 def get_user_id():
-    if user_id is not None:
-        return user_id
+    if login_user_id is not None:
+        return login_user_id
     else:
         print('---用戶尚未登陸,或取得id失敗---')
         return 0
