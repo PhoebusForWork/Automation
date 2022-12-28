fd914d23d6cfbf53386af37c00179df8195c5264
phoebus liu <phoebusliu@test.com>
2022-12-29 00:26:57 +0800

    將連結資訊拉出寫在config內

--- utils/redis_utils.py
+++ utils/redis_utils.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
 import redis
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+plt_host = config['redis_connection']['plt_host']
+cs_host = config['redis_connection']['cs_host']
+plt_port = config['redis_connection']['plt_port']
+cs_port = config['redis_connection']['cs_port']
+plt_password = config['redis_connection']['plt_password']
+cs_password = config['redis_connection']['cs_password']
 
 
 class Redis:
-    def __init__(self, host='127.0.0.1', platform='plt', password='8LvIV2Df8l', select=None):
-        port = None
+    def __init__(self, platform='plt', select=None):
+        self.host = None
+        self.port = None
+        self.password = None
         if platform == 'plt':
-            port = '6379'
-            password = '8LvIV2Df8l'
+            self.host = plt_host
+            self.port = plt_port
+            self.password = plt_password
         elif platform == 'cs':
-            port = '6380'
-            password = 'mG3G9o5jSp'
+            self.host = cs_host
+            self.port = cs_port
+            self.password = cs_password
         else:
             raise "Platform Error"
         self.conn = redis.Redis(
-            host=host, password=password, port=port, db=select)
+            host=self.host, password=self.password, port=self.port, db=select)
 
     def keys(self, keyword='*'):
         target = self.conn.keys(keyword)
