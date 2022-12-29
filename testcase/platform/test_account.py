8e942f11eed05ad1198896c860079e1cb31fcc98
gramcharliechan <cipherlab.charlie@gmail.com>
2022-12-29 14:40:09 +0800

    新增帳號管理測試案例

--- testcase/platform/test_account.py
+++ testcase/platform/test_account.py
@@ -19,155 +19,155 @@ testData = td.read_json5('test_account.json5')
 # test_case #
 #############
 
-# @allure.feature("頁面結構")
-# @allure.story("權限列表")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case( 'test_authority_permission'))
-# def test_authority_permission(test, getPltLoginToken):
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
-#                            test["params"], token=getPltLoginToken)
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test["keyword"] in resp.text
-#
-#
-# @allure.feature("頁面結構")
-# @allure.story("權限總列表")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case('test_authority_list'))
-# def test_authority_list(test, getPltLoginToken):
-#
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
-#                            test["params"], token=getPltLoginToken)
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test["keyword"] in resp.text
-#
-#
-# @allure.feature("頁面結構")
-# @allure.story("選單樹列表")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case( 'test_authority_menu'))
-# def test_authority_menu(test, getPltLoginToken):
-#
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test['req_url'], test["json"],
-#                            test["params"], token=getPltLoginToken)
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test["keyword"] in resp.text
-#
-#
-#
-# @allure.feature("平台模組")
-# @allure.story("取得平台資訊")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case('test_platform'))
-# def test_platform(test, getPltLoginToken):
-#
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
-#                            test["params"], token=getPltLoginToken)
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test["keyword"] in resp.text
-#
-#
-#
-# @allure.feature("帳號登入登出模組")
-# @allure.story("新帳號重設密碼")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case('test_account_login_resetPassword'))
-# def test_account_login_resetPassword(test, getPltLoginToken):
-#
-#     json_replace = td.replace_json(test["json"], test["target"])
-#
-#     print(json)
-#     print(json_replace)
-#     if json_replace["account"] == "建立新帳號":
-#         new_account = accountAdmin()
-#         json_replace["account"] = str(new_account.add_account_auto(platToken=getPltLoginToken))
-#     print(json)
-#     print(json_replace)
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test["req_url"], json_replace,
-#                            test["params"], token=getPltLoginToken)
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test["keyword"] in resp.text
-#
-#
-# @allure.feature("帳號登入登出模組")
-# @allure.story("帳號登出")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case('test_account_logout'))
-# def test_account_logout(test, getPltLoginToken):
-#
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
-#                            test["params"], token=getPltLoginToken)
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test["keyword"] in resp.text
-#
-#
-# @allure.feature("帳號登入登出模組")
-# @allure.story("取得圖形驗證碼")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case('test_login_imgCode'))
-# def test_login_imgCode(test, getPltLoginToken):
-#
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
-#                            test["params"], token=getPltLoginToken)
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test["keyword"] in resp.text
-#
-#
-# @allure.feature("帳號登入登出模組")
-# @allure.story("帳號登入")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case('test_account_login'))
-# def test_account_login(test, ):
-#
-#     json_replace = td.replace_json(test["json"], test["target"])  # 替換完後下面參數要用json_replace去操作
-#
-#     imgcode = PLAT_API()
-#     if json_replace['imgCode'] == "給我圖形驗證碼":
-#         json_replace['imgCode'] = str(
-#             imgcode.ImgCode(uuid=json_replace['uuid']))
-#
-#     print(json_replace['uuid'])
-#
-#     api = API_Controller()
-#     resp = api.HttpsClient(test["req_method"], test["req_url"], json_replace,
-#                            test["params"])
-#
-#     assert resp.status_code == test["code_status"], resp.text
-#     assert test['keyword'] in resp.text
-#
-#
-#
-# @allure.feature("組織結構") ###roleId參數會影響resp selected的值(true/false)
-# @allure.story("節點列表")
-# @allure.title("{scenario}")
-# @pytest.mark.parametrize("test", td.get_case('test_dept_list'))
-# def test_dept_list(test, getPltLoginToken):
-#     json_replace = td.replace_json(test['json'], test['target'])
-#     api = API_Controller()
-#     resp = api.HttpsClient(test['req_method'], test['req_url'], json_replace,
-#                            test['params'], token=getPltLoginToken)
-#     assert resp.status_code == test['code_status'], resp.text
-#     if "[roleId]基本查詢" in test['scenario']:
-#         resp = resp.json()
-#         for d in resp['data']:
-#             if d['id'] == 2:
-#                 assert d["selected"] is True
-#     else:
-#         assert test['keyword'] in resp.text
+@allure.feature("頁面結構")
+@allure.story("權限列表")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case( 'test_authority_permission'))
+def test_authority_permission(test, getPltLoginToken):
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
+                           test["params"], token=getPltLoginToken)
+    assert resp.status_code == test["code_status"], resp.text
+    assert test["keyword"] in resp.text
+
+
+@allure.feature("頁面結構")
+@allure.story("權限總列表")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case('test_authority_list'))
+def test_authority_list(test, getPltLoginToken):
+
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
+                           test["params"], token=getPltLoginToken)
+    assert resp.status_code == test["code_status"], resp.text
+    assert test["keyword"] in resp.text
+
+
+@allure.feature("頁面結構")
+@allure.story("選單樹列表")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case( 'test_authority_menu'))
+def test_authority_menu(test, getPltLoginToken):
+
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test['req_url'], test["json"],
+                           test["params"], token=getPltLoginToken)
+    assert resp.status_code == test["code_status"], resp.text
+    assert test["keyword"] in resp.text
+
+
+
+@allure.feature("平台模組")
+@allure.story("取得平台資訊")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case('test_platform'))
+def test_platform(test, getPltLoginToken):
+
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
+                           test["params"], token=getPltLoginToken)
+    assert resp.status_code == test["code_status"], resp.text
+    assert test["keyword"] in resp.text
+
+
+
+@allure.feature("帳號登入登出模組")
+@allure.story("新帳號重設密碼")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case('test_account_login_resetPassword'))
+def test_account_login_resetPassword(test, getPltLoginToken):
+
+    json_replace = td.replace_json(test["json"], test["target"])
+
+    print(json)
+    print(json_replace)
+    if json_replace["account"] == "建立新帳號":
+        new_account = accountAdmin()
+        json_replace["account"] = str(new_account.add_account_auto(platToken=getPltLoginToken))
+    print(json)
+    print(json_replace)
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test["req_url"], json_replace,
+                           test["params"], token=getPltLoginToken)
+    assert resp.status_code == test["code_status"], resp.text
+    assert test["keyword"] in resp.text
+
+
+@allure.feature("帳號登入登出模組")
+@allure.story("帳號登出")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case('test_account_logout'))
+def test_account_logout(test, getPltLoginToken):
+
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
+                           test["params"], token=getPltLoginToken)
+    assert resp.status_code == test["code_status"], resp.text
+    assert test["keyword"] in resp.text
+
+
+@allure.feature("帳號登入登出模組")
+@allure.story("取得圖形驗證碼")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case('test_login_imgCode'))
+def test_login_imgCode(test, getPltLoginToken):
+
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test["req_url"], test["json"],
+                           test["params"], token=getPltLoginToken)
+    assert resp.status_code == test["code_status"], resp.text
+    assert test["keyword"] in resp.text
+
+
+@allure.feature("帳號登入登出模組")
+@allure.story("帳號登入")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case('test_account_login'))
+def test_account_login(test, ):
+
+    json_replace = td.replace_json(test["json"], test["target"])  # 替換完後下面參數要用json_replace去操作
+
+    imgcode = PLAT_API()
+    if json_replace['imgCode'] == "給我圖形驗證碼":
+        json_replace['imgCode'] = str(
+            imgcode.ImgCode(uuid=json_replace['uuid']))
+
+    print(json_replace['uuid'])
+
+    api = API_Controller()
+    resp = api.HttpsClient(test["req_method"], test["req_url"], json_replace,
+                           test["params"])
+
+    assert resp.status_code == test["code_status"], resp.text
+    assert test['keyword'] in resp.text
+
+
+
+@allure.feature("組織結構") ###roleId參數會影響resp selected的值(true/false)
+@allure.story("節點列表")
+@allure.title("{scenario}")
+@pytest.mark.parametrize("test", td.get_case('test_dept_list'))
+def test_dept_list(test, getPltLoginToken):
+    json_replace = td.replace_json(test['json'], test['target'])
+    api = API_Controller()
+    resp = api.HttpsClient(test['req_method'], test['req_url'], json_replace,
+                           test['params'], token=getPltLoginToken)
+    assert resp.status_code == test['code_status'], resp.text
+    if "[roleId]基本查詢" in test['scenario']:
+        resp = resp.json()
+        for d in resp['data']:
+            if d['id'] == 2:
+                assert d["selected"] is True
+    else:
+        assert test['keyword'] in resp.text
 
 @allure.feature("組織結構")
 @allure.story("新增節點")
