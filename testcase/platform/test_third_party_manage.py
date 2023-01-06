316d1edbfe4f977dcee120d9e74e7109231233d6
tina <sqirrelse@gmail.com>
2023-01-06 18:49:52 +0800

    [新增三方接口管理測試案列]移除測試時的註解的查詢程式

--- testcase/platform/test_third_party_manage.py
+++ testcase/platform/test_third_party_manage.py
@@ -21,21 +21,21 @@ testData = td.read_json5('test_third_party_manage.json5')
 #############
 
 class Test_Thrid_Party_Manage():
-    # @staticmethod
-    # @allure.feature("三方接口管理")
-    # @allure.story("獲取三方接口列表")
-    # @allure.title("{test[scenario]}")
-    # @pytest.mark.parametrize("test", td.get_case('get_third_party_manage'))
-    # def test_get_third_party_manage(test, getPltLoginToken):
-    #     api = API_Controller()
-    #     resp = api.HttpsClient(test['req_method'], test['req_url'], test['json'],
-    #                            test['params'], token=getPltLoginToken)
-    #     assert resp.status_code == test['code_status'], resp.text
-    #     # check multi-type
-    #     if "所有資料" in test['scenario']:
-    #         assert set(test['keyword']).issubset(jsonpath.jsonpath(resp.json(), '$..type'))
-    #     else:
-    #         assert test['keyword'] in resp.text
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("獲取三方接口列表")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", td.get_case('get_third_party_manage'))
+    def test_get_third_party_manage(test, getPltLoginToken):
+        api = API_Controller()
+        resp = api.HttpsClient(test['req_method'], test['req_url'], test['json'],
+                               test['params'], token=getPltLoginToken)
+        assert resp.status_code == test['code_status'], resp.text
+        # check multi-type
+        if "所有資料" in test['scenario']:
+            assert set(test['keyword']).issubset(jsonpath.jsonpath(resp.json(), '$..type'))
+        else:
+            assert test['keyword'] in resp.text
 
     @staticmethod
     @allure.feature("三方接口管理")
