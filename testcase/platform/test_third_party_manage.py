371f28986dc26e3c3e039f89149e020a1e0c7a58
tina <sqirrelse@gmail.com>
2023-01-06 18:26:47 +0800

    [新增三方接口管理測試案列]調整1.id改由json_replace抓取2.編輯後查無資料增加防呆

--- testcase/platform/test_third_party_manage.py
+++ testcase/platform/test_third_party_manage.py
@@ -21,21 +21,21 @@ testData = td.read_json5('test_third_party_manage.json5')
 #############
 
 class Test_Thrid_Party_Manage():
-    @staticmethod
-    @allure.feature("三方接口管理")
-    @allure.story("獲取三方接口列表")
-    @allure.title("{test[scenario]}")
-    @pytest.mark.parametrize("test", td.get_case('get_third_party_manage'))
-    def test_get_third_party_manage(test, getPltLoginToken):
-        api = API_Controller()
-        resp = api.HttpsClient(test['req_method'], test['req_url'], test['json'],
-                               test['params'], token=getPltLoginToken)
-        assert resp.status_code == test['code_status'], resp.text
-        # check multi-type
-        if "所有資料" in test['scenario']:
-            assert set(test['keyword']).issubset(jsonpath.jsonpath(resp.json(), '$..type'))
-        else:
-            assert test['keyword'] in resp.text
+    # @staticmethod
+    # @allure.feature("三方接口管理")
+    # @allure.story("獲取三方接口列表")
+    # @allure.title("{test[scenario]}")
+    # @pytest.mark.parametrize("test", td.get_case('get_third_party_manage'))
+    # def test_get_third_party_manage(test, getPltLoginToken):
+    #     api = API_Controller()
+    #     resp = api.HttpsClient(test['req_method'], test['req_url'], test['json'],
+    #                            test['params'], token=getPltLoginToken)
+    #     assert resp.status_code == test['code_status'], resp.text
+    #     # check multi-type
+    #     if "所有資料" in test['scenario']:
+    #         assert set(test['keyword']).issubset(jsonpath.jsonpath(resp.json(), '$..type'))
+    #     else:
+    #         assert test['keyword'] in resp.text
 
     @staticmethod
     @allure.feature("三方接口管理")
@@ -52,8 +52,10 @@ class Test_Thrid_Party_Manage():
         assert test['keyword'] in resp.text
         # query and check data after editing
         if resp.status_code == 200:
-            id = str(test['json'][0]['id']) if 'id' not in test['target'] else str(test['target']['id'])
+            id = str(json_replace[0]['id'])
             req = jsonpath.jsonpath(thirdPartyManage().getThirdInterface(plat_token=getPltLoginToken),
                                     f'$.data[?(@.id == {id} )]')
+            # 編輯時若ID不存在，目前會回傳success，但查詢結果無資料，針對此特例排除
+            if req is False:
+               req = [{"error":f"查無此id={id}資料"}]
             assert set(test['target'].items()).issubset(req[0].items())
-
