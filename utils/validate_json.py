a73a0c4074cada9456e89bee371c76b0cba8d56d
phoebus liu <phoebusliu@test.com>
2023-02-07 16:36:17 +0800

    移除不需要的funtion

--- utils/validate_json.py
+++ utils/validate_json.py
@@ -3,18 +3,8 @@ import jsonschema
 from jsonschema import validate
 
 
-def get_schema():
-    """This function loads the given schema available"""
-    with open('user_schema.json', 'r') as file:
-        schema = json.load(file)
-    return schema
-
-
 def validate_json(instance, schema):
     """REF: https://json-schema.org/ """
-    # Describe what kind of json you expect.
-    # execute_api_schema = get_schema()
-
     try:
         validate(instance=instance, schema=schema)
     except jsonschema.exceptions.ValidationError as err:
