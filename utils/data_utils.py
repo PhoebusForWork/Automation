5f30770d14c46a4cfac6c0c686713b81527f160e
phoebus liu <phoebusliu@test.com>
2023-02-20 09:36:06 +0800

    redis哨兵模式初版

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -122,6 +122,10 @@ class EnvReader:
             self.REDIS_CS_PORT = config['redis_connection']['redis_cs_port']
             self.REDIS_PLT_PASSWORD = config['redis_connection']['redis_plt_password']
             self.REDIS_CS_PASSWORD = config['redis_connection']['redis_cs_password']
+            self.REDIS_PLT_SENTINEL_LIST = eval(
+                config['redis_connection']['redis_plt_sentinel_list'])
+            self.REDIS_CS_SENTINEL_LIST = eval(
+                config['redis_connection']['redis_cs_sentinel_list'])
         else:
             self.REDIS_PLT_HOST = os.getenv('REDIS_PLT_HOST')
             self.REDIS_CS_HOSTcs_host = os.getenv('REDIS_CS_HOST')
@@ -129,6 +133,8 @@ class EnvReader:
             self.REDIS_CS_PORT = os.getenv('REDIS_CS_PORT')
             self.REDIS_PLT_PASSWORD = os.getenv('REDIS_PLT_PASSWORD')
             self.REDIS_CS_PASSWORD = os.getenv('REDIS_CS_PASSWORD')
+            self.REDIS_PLT_SENTINEL_LIST = os.getenv('REDIS_PLT_SENTINEL_LIST')
+            self.REDIS_CS_SENTINEL_LIST = os.getenv('REDIS_CS_SENTINEL_LIST')
 
     def __load_cs_test_account(self):
         self.CS_ACCOUNT = config['cs_account']['account']
--- utils/redis_utils.py
+++ utils/redis_utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import redis
+from redis.sentinel import Sentinel
 from utils.data_utils import EnvReader
 
 
@@ -8,6 +9,8 @@ plt_host = env.REDIS_PLT_HOST
 cs_host = env.REDIS_CS_HOST
 plt_port = env.REDIS_PLT_PORT
 cs_port = env.REDIS_CS_PORT
+plt_sentinel_list = env.REDIS_PLT_SENTINEL_LIST
+cs_sentinel_list = env.REDIS_PLT_SENTINEL_LIST
 plt_password = env.REDIS_PLT_PASSWORD
 cs_password = env.REDIS_CS_PASSWORD
 
@@ -37,6 +40,28 @@ class Redis:
     def set_value(self, key, value):
         target = self.conn.set(key, value)
         return target
+
+
+class RedisSentinel:
+    def __init__(self, platform='plt', name='mymaster', select=None):
+        if platform == 'plt':
+            self.sentinel_list = plt_sentinel_list
+            self.password = plt_password
+        elif platform == 'cs':
+            self.sentinel_list = cs_sentinel_list
+            self.password = cs_password
+        else:
+            raise "Platform Error"
+        setting = {
+            'service_name': name,
+            "socket_timeout": 60,
+            "password": self.password,
+            "db": select
+        }
+        self.sentinel = Sentinel(
+            sentinels=self.sentinel_list, socket_timeout=60)
+        self.master = self.sentinel.master_for(**setting)
+        self.slave = self.sentinel.slave_for(**setting)
 
 
 if __name__ == '__main__':
