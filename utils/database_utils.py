cf217005c7080faf7a91e7f76a2b9554ff13c382
phoebus liu <phoebusliu@test.com>
2023-02-22 12:52:05 +0800

    寫es工具(尚未完成)

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
-import psycopg2
-from pymongo import MongoClient
-from utils.data_utils import EnvReader
+import os
+import sys
 
+sys.path.append(os.path.abspath('.'))
+from utils.data_utils import EnvReader
+from elasticsearch import Elasticsearch
+import psycopg2
+import json
 
 env = EnvReader()
 plt_host = env.POSTGRES_PLT_HOST
@@ -31,8 +35,11 @@ class Postgresql:
             self.password = cs_password
         else:
             raise "platform Error"
-        self.db = psycopg2.connect(
-            database=self.database, user=self.user, password=self.password, host=self.host, port=self.port)
+        self.db = psycopg2.connect(database=self.database,
+                                   user=self.user,
+                                   password=self.password,
+                                   host=self.host,
+                                   port=self.port)
 
     def select_sql(self, sql):
         self.cursor = self.db.cursor()
@@ -58,10 +65,49 @@ class Postgresql:
             self.db.close()
 
 
+class ES:
+
+    def __init__(self):
+        self.host = 'localhost'
+        self.port = '9200'
+        self.user = 'developer'
+        self.password = 'sRQV9foYA7cKEUh'
+
+        self.es = Elasticsearch(hosts=self.host,
+                                http_auth=(self.user, self.password),
+                                port=self.port)
+
+    def get_index(self):
+        target = self.es.indices.get_alias().keys()
+        return target
+
+    def query(self, index, query_json, scroll='5m', size=100):
+        target = self.es.search(index=index,
+                                query=query_json,
+                                scroll=scroll,
+                                size=size)
+        return target
+
+    def add_data(self, index, json_data):
+        self.es.index(index=index, body=json_data)
+
+    def update_data():
+        pass
+
+    def del_data():
+        pass
+
+
 if __name__ == '__main__':
     # database 是指定要查詢的庫
-    dbHandle = Postgresql(database='cs_user', platform="cs")
-    data = dbHandle.select_sql(
-        "select * from cs_user.vs_user where id = 28;")
-    print(data)
-    pass
+    def printJson(func):
+        print(json.dumps(func, sort_keys=True, indent=4,
+                         separators=(',', ':')))
+
+    test = ES()
+    abc = {"match": {"user_id": "66"}}
+    t = test.query(index='vs_wallet_log', query_json=abc)
+    printJson(t)
+    # a = test.get_index()
+    # print(a)
+    # print(json.dumps(a,sort_keys=True,indent=4,separators=(',',':')))
