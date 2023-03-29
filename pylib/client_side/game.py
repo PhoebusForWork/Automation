77fab42fc87d2f6f4ec007d4da33859162e67456
mimiclin <mimiclin.gram@gmail.com>
2023-03-29 14:25:42 +0800

    revise client_side/game

--- pylib/client_side/game.py
+++ pylib/client_side/game.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from pylib.client_side.webApiBase import WebAPI
+from utils.api_utils import KeywordArgument
 from utils.data_utils import EnvReader
 
 env = EnvReader()
@@ -8,85 +9,84 @@ web_host = env.WEB_HOST
 
 class Game(WebAPI):
     # 遊戲列表
-    def get_game_list(self, web_token=None,
-                      ignoreGameIds=[], os_type="WEB"  # (WEB/H5/ANDROID/IOS)
-                      ):
+    def get_game_list(
+            self, web_token=None, ignoreGameIds=[],
+            os_type="WEB"  # (WEB/H5/ANDROID/IOS)
+    ):
         if web_token is not None:
             self.request_session.headers.update({"token": str(web_token)})
-        response = self.request_session.get(web_host+"/v1/game/list",
-                               json={},
-                               params={"ignoreGameIds": ignoreGameIds,
-                                       "os-type": os_type}
-                               )
-        self.print_response(response)
+
+        request_body = {
+            "method": "get",
+            "url": "/v1/game/list",
+            "params": KeywordArgument.body_data()
+        }
+
+        response = self.send_request(**request_body)
         return response.json()
 
-    def get_game_usual(self, web_token=None,
-                       ):  # 常用遊戲列表
+    # 常用遊戲列表
+    def get_game_usual(self, web_token=None):
         if web_token is not None:
             self.request_session.headers.update({"token": str(web_token)})
-        response = self.request_session.get(web_host+"/v1/game/usual",
-                               json={},
-                               params={}
-                               )
-        self.print_response(response)
+
+        request_body = {
+            "method": "get",
+            "url": "/v1/game/usual"
+        }
+
+        response = self.send_request(**request_body)
         return response.json()
 
-    def game_redirect(self, web_token=None,
-                      gameCode='AWC_LIVE_SEXY', backUrl="https://tttint.onlinegames22.com/wallet/login", subGameCode=None, os_type="", token=None
-                      ):  # 遊戲跳轉
+    # 遊戲跳轉
+    def game_redirect(
+            self, web_token=None, gameCode='AWC_LIVE_SEXY',
+            backUrl="https://tttint.onlinegames22.com/wallet/login", subGameCode=None, os_type="", token=None
+    ):
         if web_token is not None:
             self.request_session.headers.update({"token": str(web_token)})
-        response = self.request_session.get(web_host+"/v1/game/redirect/{}/url".format(gameCode),
-                               json={},
-                               params={"gameCode": gameCode, "backUrl": backUrl,
-                                       "subGameCode": subGameCode, "os-type": os_type, "token": token}
-                               )
-        self.print_response(response)
+
+        request_body = {
+            "method": "get",
+            "url": f"/v1/game/redirect/{gameCode}/url",
+            "params": KeywordArgument.body_data()
+        }
+
+        response = self.send_request(**request_body)
         return response.json()
 
 
 class GameOrder(WebAPI):
-
-    def get_game_order(self, web_token=None,
-                       orderType=2, gameCode=None,
-                       settleStatus=None,
-                       startTime="2022-10-17T00:00:00Z", endTime="2022-10-17T23:59:59Z",
-                       page=None, size=None
-                       ):  # 查詢注單
+    # 查詢注單
+    def get_game_order(
+            self, web_token=None, orderType=2, gameCode=None, settleStatus=None,
+            startTime="2022-10-17T00:00:00Z", endTime="2022-10-17T23:59:59Z", page=None, size=None
+    ):
         if web_token is not None:
             self.request_session.headers.update({"token": str(web_token)})
-        response = self.request_session.get(web_host+"/v1/game/order",
-                               json={},
-                               params={
-                                   "orderType": orderType,
-                                   "gameCode": gameCode,
-                                   "settleStatus": settleStatus,
-                                   "startTime": startTime,
-                                   "endTime": endTime,
-                                   "page": page,
-                                   "size": size,
-                               }
-                               )
-        self.print_response(response)
+
+        request_body = {
+            "method": "get",
+            "url": "/v1/game/order",
+            "params": KeywordArgument.body_data()
+        }
+
+        response = self.send_request(**request_body)
         return response.json()
 
-    def get_game_order_summary(self, web_token=None,
-                               orderType=2, gameCode=None,
-                               settleStatus=None,
-                               startTime="2022-10-17T00:00:00+08:00", endTime="2022-10-17T23:59:59+08:00",
-                               ):  # 注單總和查詢
+    # 注單總和查詢
+    def get_game_order_summary(
+            self, web_token=None, orderType=2, gameCode=None, settleStatus=None,
+            startTime="2022-10-17T00:00:00+08:00", endTime="2022-10-17T23:59:59+08:00"
+    ):
         if web_token is not None:
             self.request_session.headers.update({"token": str(web_token)})
-        response = self.request_session.get(web_host+"/v1/game/order/summary",
-                               json={},
-                               params={
-                                   "orderType": orderType,
-                                   "gameCode": gameCode,
-                                   "settleStatus": settleStatus,
-                                   "startTime": startTime,
-                                   "endTime": endTime,
-                               }
-                               )
-        self.print_response(response)
+
+        request_body = {
+            "method": "get",
+            "url": "/v1/game/order/summary",
+            "params": KeywordArgument.body_data()
+        }
+
+        response = self.send_request(**request_body)
         return response.json()
