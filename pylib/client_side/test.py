767ca74a23025f9b6cf6f9e5d80c85a5d10c9603
phoebus liu <phoebusliu@test.com>
2023-05-04 15:42:01 +0800

    調整前台測試接口與response列印方法

---
+++ pylib/client_side/test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from ..client_side.webApiBase import WebAPI
+from utils.data_utils import EnvReader
+
+
+env = EnvReader()
+web_host = env.WEB_HOST
+
+# test-controller
+class TransferMock(WebAPI):
+    # 是否檢核註冊次數
+    def set_env(self, is_pro: bool):
+        request_body = {
+            "method": "put",
+            "url": "/v1/test/evn",
+            "params": {"isPro": is_pro}
+        }
+        response = self.send_request(**request_body)
+        return response
