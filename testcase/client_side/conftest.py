ee63c93dd69f04ebc403eb4df149223b59bd84be
phoebus liu <phoebusliu@test.com>
2023-05-17 09:14:12 +0800

    初版QA環境內容暫存

--- testcase/client_side/conftest.py
+++ testcase/client_side/conftest.py
@@ -5,10 +5,8 @@ from pylib.client_side.webApiBase import WebAPI
 from utils.data_utils import EnvReader
 
 env = EnvReader()
-cs_account = env.CS_TEST_ACCOUNT
-cs_password = env.CS_TEST_PASSWORD
-login_user_id = None
-
+cs_account = env.CS_ACCOUNT
+cs_password = env.CS_PASSWORD
 
 @pytest.fixture(scope="module")
 def get_client_side_token(username=cs_account, password=cs_password):
@@ -18,17 +16,27 @@ def get_client_side_token(username=cs_account, password=cs_password):
         token = resp.json()['data']['token']
         global login_user_id
         login_user_id = resp.json()['data']['userId']
+        print(login_user_id)
     except Exception as ex:
         logging.error('登錄失敗！接口返回:{}'.format(resp.text))
         traceback.print_tb(ex)
     logging.info("登錄成功,token為 : {}".format(token))
-    return token
+    return token, login_user_id
 
-
-@pytest.fixture(scope="session")
-def get_user_id():
-    if login_user_id is not None:
-        return login_user_id
+@pytest.fixture(scope="module")
+def get_user_token(get_client_side_token):
+    token, _ = get_client_side_token
+    if token is not None:
+        return token
     else:
-        print('---用戶尚未登陸,或取得id失敗---')
+        print('---用戶尚未登陸，或取得id失敗---')
+        return 0
+
+@pytest.fixture(scope="module")
+def get_user_id(get_client_side_token):
+    _, user_id = get_client_side_token
+    if user_id is not None:
+        return user_id
+    else:
+        print('---用戶尚未登陸，或取得id失敗---')
         return 0
--- testcase/client_side/test_transfer.py
+++ testcase/client_side/test_transfer.py
@@ -1,132 +1,167 @@
 import pytest
 import allure
-from utils.data_utils import TestDataReader
+from utils.data_utils import TestDataReader, ResponseVerification
 from utils.api_utils import API_Controller
 from utils.postgres_utils import UserWallet
 from pylib.client_side.wallet import GameTransfer
 
-td = TestDataReader()
-td.read_json5('test_wallet.json5', file_side='cs')
+test_data = TestDataReader()
+test_data.read_json5('test_wallet.json5', file_side='cs')
 
 
 ######################
 #  setup & teardown  #
 ######################
 
-@pytest.fixture(scope="class")  #
-def reset_user_wallet_for_deposit(get_client_side_token):
-    do_withdraw = GameTransfer()
-    do_withdraw.wallet_game_transfer_withdraw_all(
-        web_token=get_client_side_token)
+@pytest.fixture(scope="class")
+def reset_user_wallet_for_deposit(get_user_token):
+    do_withdraw = GameTransfer(token=get_user_token)
+    do_withdraw.wallet_game_transfer_withdraw_all()
     reset = UserWallet()
     reset.reset(balance=100)
     yield
-    do_withdraw.wallet_game_transfer_withdraw_all(
-        web_token=get_client_side_token)
+    do_withdraw.wallet_game_transfer_withdraw_all()
 
 
-@pytest.fixture(scope="class")  #
-def reset_user_wallet_for_withdraw(get_client_side_token, get_user_id):
-    do_withdraw = GameTransfer()
-    do_withdraw.wallet_game_transfer_withdraw_all(
-        web_token=get_client_side_token)
+@pytest.fixture(scope="class")
+def reset_user_wallet_for_withdraw(get_user_token, get_user_id):
+    do_withdraw = GameTransfer(token=get_user_token)
+    do_withdraw.wallet_game_transfer_withdraw_all()
     reset = UserWallet()
     reset.reset(balance=100, user_id=get_user_id)
     do_withdraw.wallet_game_transfer_deposit(
-        web_token=get_client_side_token, channelCode='AWC', amount=100)
+        channelCode='AI', amount=100)
 
 
-@pytest.fixture(scope="class")  #
-def reset_user_wallet_for_withdraw_all(get_client_side_token, get_user_id):
-    do_withdraw = GameTransfer()
-    do_withdraw.wallet_game_transfer_withdraw_all(
-        web_token=get_client_side_token)
+@pytest.fixture(scope="class")
+def reset_user_wallet_for_withdraw_all(get_user_token, get_user_id):
+    do_withdraw = GameTransfer(token=get_user_token)
+    do_withdraw.wallet_game_transfer_withdraw_all()
     reset = UserWallet()
     reset.reset(balance=150, user_id=get_user_id)
     do_withdraw.wallet_game_transfer_deposit(
-        web_token=get_client_side_token, channelCode='AWC', amount=50)
+        channelCode='AI', amount=50)
 
 
 ######################
 #      testCase      #
 ######################
 
-
-@allure.feature("錢包管理")
-@allure.story("顯示中心錢包及各遊戲錢包金額和渠道狀態")
-@allure.title("{test[scenario]}")
-@pytest.mark.parametrize("test", td.get_case('get_wallet_user_info'))
-def test_get_wallet_user_info(test, get_client_side_token):
-
-    api = API_Controller(platform='cs')
-    resp = api.send_request(test['req_method'], test['req_url'], test['json'],
-                            test['params'], token=get_client_side_token)
-    assert resp.status_code == test['code_status'], resp.text
-    assert test['keyword'] in resp.text
-
-
-class TestWithdrawAll:
+# 轉帳操作
+class TestGameTransfer:
+    # 從指定遊戲渠道轉錢回中心錢包
     @staticmethod
-    @allure.feature("錢包管理")
-    @allure.story("一鍵回收")
+    @allure.feature("轉帳操作")
+    @allure.story("從指定遊戲渠道轉錢回中心錢包")
     @allure.title("{test[scenario]}")
-    @pytest.mark.parametrize("test", td.get_case('wallet_game_transfer_withdraw_all'))
-    def test_wallet_game_transfer_withdraw_all(test, get_client_side_token, reset_user_wallet_for_withdraw_all):
+    # @pytest.mark.test
+    @pytest.mark.usefixtures("reset_user_wallet_for_withdraw")
+    @pytest.mark.parametrize("test", test_data.get_case('wallet_game_transfer_withdraw'))
+    def test_wallet_game_transfer_withdraw(test, get_user_token):
+        json_replace = test_data.replace_json(test["json"], test["target"])
+        api = API_Controller(platform='cs')
+        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+                                test['params'], token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
 
+    # 一鍵收回
+    @staticmethod
+    @allure.feature("轉帳操作")
+    @allure.story("一鍵收回")
+    @allure.title("{test[scenario]}")
+    # @pytest.mark.test
+    @pytest.mark.usefixtures("reset_user_wallet_for_withdraw_all")
+    @pytest.mark.parametrize("test", test_data.get_case('wallet_game_transfer_withdraw_all'))
+    def test_wallet_game_transfer_withdraw_all(test, get_user_token):
+        params_replace = test_data.replace_json(test["params"], test["target"])
         api = API_Controller(platform='cs')
         resp = api.send_request(test['req_method'], test['req_url'], test['json'],
-                                test['params'], token=get_client_side_token)
-        assert resp.status_code == test['code_status'], resp.text
-        assert test['keyword'] in resp.text
+                                params_replace, token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
 
-
-class TestDeposit:
+    # 將錢轉出至遊戲渠道
     @staticmethod
-    @allure.feature("錢包管理")
+    @allure.feature("轉帳操作")
     @allure.story("將錢轉出至遊戲渠道")
     @allure.title("{test[scenario]}")
-    @pytest.mark.parametrize("test", td.get_case('wallet_game_transfer_deposit'))
-    def test_wallet_game_transfer_deposit(test, get_client_side_token, get_user_id, reset_user_wallet_for_deposit):
-
+    # @pytest.mark.test
+    @pytest.mark.usefixtures("reset_user_wallet_for_deposit")
+    @pytest.mark.parametrize("test", test_data.get_case('wallet_game_transfer_deposit'))
+    def test_wallet_game_transfer_deposit(test, get_user_token):
+        json_replace = test_data.replace_json(test["json"], test["target"])
         api = API_Controller(platform='cs')
-        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
-                                test['params'], token=get_client_side_token)
-        assert resp.status_code == test['code_status'], resp.text
-        assert test['keyword'] in resp.text
-        if resp.status_code == 200:  # 轉帳成功額外確認資料庫是否正確
-            assert UserWallet.check_deposit(user_id=get_user_id,
-                                            check_amount=test['json']['amount'], channel=test['json']['channelCode']) is True
+        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+                                test['params'], token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
 
-
-class TestWithdraw:
+    # 顯示中心錢包及各遊戲錢包金額和渠道狀態
     @staticmethod
-    @allure.feature("錢包管理")
-    @allure.story("從指定遊戲渠道轉回中心錢包")
+    @allure.feature("轉帳操作")
+    @allure.story("顯示中心錢包及各遊戲錢包金額和渠道狀態")
     @allure.title("{test[scenario]}")
-    @pytest.mark.parametrize("test", td.get_case('wallet_game_transfer_withdraw'))
-    def test_wallet_game_transfer_withdraw(test, get_client_side_token, get_user_id, reset_user_wallet_for_withdraw):
-
+    # @pytest.mark.test
+    @pytest.mark.parametrize("test", test_data.get_case('get_wallet_user_info'))
+    def test_get_wallet_user_info(test, get_user_token):
+        params_replace = test_data.replace_json(test["params"], test["target"])
         api = API_Controller(platform='cs')
         resp = api.send_request(test['req_method'], test['req_url'], test['json'],
-                                test['params'], token=get_client_side_token)
-        assert resp.status_code == test['code_status'], resp.text
-        assert test['keyword'] in resp.text
-        if resp.status_code == 200:  # 轉帳成功額外確認資料庫是否正確
-            assert UserWallet.check_withdraw(user_id=get_user_id,
-                                             check_amount=test['json']['amount'], channel=test['json']['channelCode']) is True
+                                params_replace, token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
 
 
-@allure.feature("錢包管理")  # 不能使用噴錯
-@allure.story("取得使用者資金明細")
-@allure.title("{test[scenario]}")
-@pytest.mark.parametrize("test", td.get_case('get_wallet_front_user_fund'))
-def test_get_wallet_front_user_fund(test, get_client_side_token):
+# 錢包操作
+class TestFrontUser:
+    # 取得使用者訂單資訊
+    @staticmethod
+    @allure.feature("錢包操作")
+    @allure.story("取得使用者訂單資訊")
+    @allure.title("{test[scenario]}")
+    # @pytest.mark.test
+    @pytest.mark.parametrize("test", test_data.get_case('get_trade_info'))
+    def test_get_trade_info(test, get_user_token):
+        json_replace = test_data.replace_json(test["json"], test["target"])
+        api = API_Controller(platform='cs')
+        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+                                test['params'], token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
 
-    json_replace = td.replace_json(test['params'], test['target'])
+    # 遊戲可回收餘額
+    @staticmethod
+    @allure.feature("錢包操作")
+    @allure.story("遊戲可回收餘額")
+    @allure.title("{test[scenario]}")
+    # @pytest.mark.test
+    @pytest.mark.parametrize("test", test_data.get_case('get_refundable_balance'))
+    def test_get_refundable_balance(test, get_user_token):
+        api = API_Controller(platform='cs')
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
+                                test['params'], token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
 
-    api = API_Controller(platform='cs')
-    resp = api.send_request(test['req_method'], test['req_url'], test['json'],
-                            json_replace, token=get_client_side_token)
+    # 取得使用者資金明細
+    @staticmethod
+    @allure.feature("錢包操作")
+    @allure.story("取得使用者資金明細")
+    @allure.title("{test[scenario]}")
+    # @pytest.mark.test
+    @pytest.mark.parametrize("test", test_data.get_case('get_wallet_front_user_fund'))
+    def test_get_wallet_front_user_fund(test, get_user_token):
+        params_replace = test_data.replace_json(test["params"], test["target"])
+        api = API_Controller(platform='cs')
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
+                                params_replace, token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
 
-    assert resp.status_code == test['code_status'], resp.text
-    assert test['keyword'] in resp.text
+    # 查詢用戶各幣別餘額
+    @staticmethod
+    @allure.feature("錢包操作")
+    @allure.story("查詢用戶各幣別餘額")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.test
+    @pytest.mark.parametrize("test", test_data.get_case('get_balance'))
+    def test_get_balance(test, get_user_token):
+        json_replace = test_data.replace_json(test["json"], test["target"])
+        api = API_Controller(platform='cs')
+        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+                                test['params'], token=get_user_token)
+        ResponseVerification.basic_assert(resp, test)
