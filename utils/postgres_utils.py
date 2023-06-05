3329aae8a0c520057313c0ce7b155343e9450301
phoebus liu <phoebusliu@test.com>
2023-06-05 16:20:47 +0800

    站點代號相關調整

--- utils/postgres_utils.py
+++ utils/postgres_utils.py
@@ -5,16 +5,16 @@ class UserWallet:
     @staticmethod
     def reset(balance="1000", user_id="29"):
         reset = Postgresql(database='wallet', platform="plt")
-        sql = f"update wallet.vs_wallet set balance = {balance} ,freeze_balance=0 where user_id = {user_id} ;"
+        sql = f"update wallet.ldpro_wallet set balance = {balance} ,freeze_balance=0 where user_id = {user_id} ;"
         reset.run_sql(sql=sql)
 
     @staticmethod
     def check_deposit(user_id="29", check_amount=100, channel="AWC"):
         check = Postgresql(database='wallet', platform='plt')
-        game_transfer = f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
+        game_transfer = f"select type,amount from wallet.ldpro_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
         data = check.select_sql(game_transfer)
         type, amount = data[0][0], data[0][1]
-        game_wallet = f"select channel_code,balance,freeze_balance from wallet.vs_game_wallet where user_id = {user_id} and channel_code = '{channel}';"
+        game_wallet = f"select channel_code,balance,freeze_balance from wallet.ldpro_game_wallet where user_id = {user_id} and channel_code = '{channel}';"
         data = check.select_sql(game_wallet)
         channel_code, balance, freeze_balance = data[0][0], data[0][1], data[0][2]
         try:
@@ -29,10 +29,10 @@ class UserWallet:
     @staticmethod
     def check_withdraw(user_id="29", check_amount=100, channel="AWC"):
         check = Postgresql(database='wallet', platform='plt')
-        game_transfer = f"select type,amount from wallet.vs_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
+        game_transfer = f"select type,amount from wallet.ldpro_game_transfer where user_id = {user_id} order by create_time desc limit 1;"
         data = check.select_sql(game_transfer)
         Type, amount = data[0][0], data[0][1]
-        game_wallet = f"select balance,freeze_balance from wallet.vs_wallet where user_id = {user_id};"
+        game_wallet = f"select balance,freeze_balance from wallet.ldpro_wallet where user_id = {user_id};"
         data = check.select_sql(game_wallet)
         balance, freeze_balance = data[0][0], data[0][1]
         try:
