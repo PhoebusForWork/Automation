a7b50b83fdf887c58cbe740f870f30106e76af16
phoebus liu <phoebusliu@test.com>
2023-06-10 09:59:04 +0800

    qa環境三方接口測試初版

--- testcase/platform/test_third_party_manage.py
+++ testcase/platform/test_third_party_manage.py
@@ -3,7 +3,8 @@ import allure
 import jsonpath
 
 from pylib.platform.thirdPartyManage import ThirdPartyManage
-from utils.data_utils import TestDataReader
+from pylib.platform.fund import UserGroup
+from utils.data_utils import TestDataReader, ResponseVerification
 from utils.api_utils import API_Controller
 from utils.json_verification import validate_json
 
@@ -13,17 +14,78 @@ test_data.read_json5('test_third_party_manage.json5')
 ######################
 #  setup & teardown  #
 ######################
-
+@pytest.fixture(scope="class")
+def check_customer_group(get_platform_token):
+    customer = ThirdPartyManage(get_platform_token)
+    target = customer.get_customer_group(groupName='客服群組名稱測試')
+    ret = jsonpath.jsonpath(target, "$..groupName")
+    if ret is False:
+        add_user_group = UserGroup(get_platform_token)
+        add_user_group.save_group(groupName='客服測試-群組類型測試-客服群組名稱測試')
 
 #############
 # test_case #
 #############
 
-
+# 三方接口管理
 class TestThirdPartyManage:
+    # 三方接口:權重更新
     @staticmethod
     @allure.feature("三方接口管理")
-    @allure.story("獲取三方接口列表")
+    @allure.story("三方接口:權重更新")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", test_data.get_case('update_weighting'))
+    def test_update_weighting(test, get_platform_token):
+        json_replace = test_data.replace_json(test["json"], test["target"])
+        api = API_Controller()
+        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+                                test['params'], token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
+
+    # 三方接口:開關更新
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("三方接口:開關更新")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", test_data.get_case('update_switched'))
+    def test_update_switched(test, get_platform_token):
+        json_replace = test_data.replace_json(test["json"], test["target"])
+        api = API_Controller()
+        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+                                test['params'], token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
+
+    # 客服接口-群組管理-查詢
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("客服接口-群組管理-查詢")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.usefixtures("check_customer_group")
+    @pytest.mark.parametrize("test", test_data.get_case('get_customer_group'))
+    def test_get_customer_group(test, get_platform_token):
+        parmas_replace = test_data.replace_json(test["params"], test["target"])
+        api = API_Controller()
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
+                                parmas_replace, token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
+
+    # 客服接口-群組管理-保存
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("客服接口-群組管理-保存")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", test_data.get_case('save_customer_group'))
+    def test_save_customer_group(test, get_platform_token):
+        json_replace = test_data.replace_json(test["json"], test["target"])
+        api = API_Controller()
+        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+                                test['params'], token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
+
+    # 三方接口:查詢
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("三方接口:查詢")
     @allure.title("{test[scenario]}")
     @pytest.mark.parametrize("test", test_data.get_case('get_third_party_manage'))
     def test_get_third_party_manage(test, get_platform_token):
@@ -36,32 +98,69 @@ class TestThirdPartyManage:
             assert set(test['keyword']).issubset(jsonpath.jsonpath(resp.json(), '$..type'))
         else:
             assert test['keyword'] in resp.text
-
         if resp.status_code == 200:
             assert validate_json(resp.json(), test['schema'])
 
+    # 三方接口:圖形驗證接口-查詢
     @staticmethod
     @allure.feature("三方接口管理")
-    @allure.story("保存三方接口列表")
+    @allure.story("三方接口:圖形驗證接口-查詢")
     @allure.title("{test[scenario]}")
-    @pytest.mark.parametrize("test", test_data.get_case('edit_third_party_manage'))
-    def test_edit_third_party_manage(test, get_platform_token):
-        json_replace = test_data.replace_json(test['json'], test['target'])
+    @pytest.mark.parametrize("test", test_data.get_case('get_captcha'))
+    def test_get_captcha(test, get_platform_token):
         api = API_Controller()
-        resp = api.send_request(test['req_method'], test['req_url'], json_replace,
+        parmas_replace = test_data.replace_json(test["params"], test["target"])
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
+                                parmas_replace, token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
+
+    # 三方接口:銀行卡接口-查詢
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("三方接口:銀行卡接口-查詢")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", test_data.get_case('get_bank_card'))
+    def test_get_bank_card(test, get_platform_token):
+        api = API_Controller()
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
                                 test['params'], token=get_platform_token)
-        # check response
-        assert resp.status_code == test['code_status'], resp.text
-        assert test['keyword'] in resp.text
-        # query and check data after editing
-        if resp.status_code == 200:
-            assert validate_json(resp.json(), test['schema'])
-            # 規則是"template"可傳入，但不會被修改，先特例處理
-            if "template" in test['scenario']:
-                pass
-            else:
-                id = str(json_replace[0]['id'])
-                req = jsonpath.jsonpath(ThirdPartyManage().get_third_interface(plat_token=get_platform_token),
-                                        f'$.data[?(@.id == {id} )]')
-                if req is not False:
-                    assert set(test['target'].items()).issubset(req[0].items())
+        ResponseVerification.basic_assert(resp, test)
+
+    # 客服接口-群組管理-最後更新時間
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("客服接口-群組管理-最後更新時間")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", test_data.get_case('get_customer_group_last_update_time'))
+    def test_get_customer_group_last_update_time(test, get_platform_token):
+        api = API_Controller()
+        parmas_replace = test_data.replace_json(test["params"], test["target"])
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
+                                parmas_replace, token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
+
+    # 客服接口-群組管理-群組類型下拉選單
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("客服接口-群組管理-群組類型下拉選單")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", test_data.get_case('get_customer_group_type'))
+    def test_get_customer_group_type(test, get_platform_token):
+        api = API_Controller()
+        params_replace = test_data.replace_json(test["params"], test["target"])
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
+                                params_replace, token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
+
+    # 客服接口-群組管理-客服下拉選單
+    @staticmethod
+    @allure.feature("三方接口管理")
+    @allure.story("客服接口-群組管理-客服下拉選單")
+    @allure.title("{test[scenario]}")
+    @pytest.mark.parametrize("test", test_data.get_case('get_customer_service'))
+    def test_get_customer_service(test, get_platform_token):
+        api = API_Controller()
+        params_replace = test_data.replace_json(test["params"], test["target"])
+        resp = api.send_request(test['req_method'], test['req_url'], test['json'],
+                                params_replace, token=get_platform_token)
+        ResponseVerification.basic_assert(resp, test)
