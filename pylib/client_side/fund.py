386c4e15cd56da7e907a1a103ff6fbb28659a45a
phoebus liu <phoebusliu@test.com>
2023-10-13 11:25:16 +0800

    新增前台充值ＡＰＩ

--- pylib/client_side/fund.py
+++ pylib/client_side/fund.py
@@ -9,14 +9,84 @@ env = EnvReader()
 web_host = env.WEB_HOST
 
 
+# 充值操作
+class Recharge(WebAPI):
+    # 充值提交
+    def submit(self, os_type=None,
+               paymentId=None, merchantId=None,
+               rechargeTypeId=None,
+               amount=None,
+               bankCode=None):
+        request_body = {
+            "method": "post",
+            "url": "/v1/fund/recharge/submit",
+            "params": {"os-type": os_type},
+            "json": KeywordArgument.body_data()
+        }
+        response = self.send_request(**request_body)
+        return response.json()
+
+    # 據用戶群組跟充值分類，獲取充值方式(點選金額後，篩選商戶)
+    def get_payment_list(self, os_type=None,
+                         amount=None,
+                         paymentId=None, rechargeTypeId=None):
+        request_body = {
+            "method": "post",
+            "url": "/v1/fund/recharge/getPaymentList",
+            "params": {"os-type": os_type},
+            "json": KeywordArgument.body_data()
+        }
+        response = self.send_request(**request_body)
+        return response.json()
+
+    # 取得使用者充值訂單資訊
+    def get_recharge_order_info(self, orderId):
+        request_body = {
+            "method": "get",
+            "url": f"/v1/fund/recharge/{orderId}",
+        }
+        response = self.send_request(**request_body)
+        return response.json()
+
+    # 获取充值类型列表
+    def get_recharge_type_list(self, os_type=None):
+        request_body = {
+            "method": "get",
+            "url": "/v1/fund/getRechargeType",
+            "params": {"os-type": os_type},
+        }
+        response = self.send_request(**request_body)
+        return response.json()
+
+    # 充值受限查詢
+    def get_recharge_limit(self):
+        request_body = {
+            "method": "get",
+            "url": "/v1/fund/getRechargeLimit",
+        }
+        response = self.send_request(**request_body)
+        return response.json()
+
+    # 获取所有充值方式列表
+    def get_payment_channels(self, os_type=None,
+                             rechargeTypeId=None):
+        request_body = {
+            "method": "get",
+            "url": "/v1/fund/getPaymentChannel",
+            "params": {"os-type": os_type},
+            "json": KeywordArgument.body_data()
+        }
+        response = self.send_request(**request_body)
+        return response.json()
+
 # 提現操作
 class Withdraw(WebAPI):
     # 發起提現
-    def submit(self, device_id, os_type,
-               amount, otpCode,
+    def submit(self, device_id=None, os_type=None,
+               amount=None, otpCode=None,
                # 提現方式 TRADITIONAL, USDT, TRANS_CENTER
-               categoryCode,
-               cardId, addressId):
+               categoryCode=None,
+               cardId=None, addressId=None):
         request_body = {
             "method": "post",
             "url": "/v1/fund/withdraw/submit",
