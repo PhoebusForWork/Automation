94c7e37a24e4d1d66bd1ef7d35be51ec353962d1
tina <sqirrelse@gmail.com>
2023-11-28 17:28:34 +0800

    1.VIP Create ACCOUNT 需先開設定風控環境 2.VIP後台設定檔日期需大於執行日期ldpro_vip_point_ratio 3.站台sync有問題，暫時用sql update plt_game.platform

--- automation_set_up.py
+++ automation_set_up.py
@@ -7,47 +7,48 @@ from scripts.automation.setup_api import platform, client_side
 from utils.xxl_job_utils import XxlJobs
 
 
-xxl = XxlJobs()
-# ####################
-#  postgres-總控模擬  #
-# ####################
-# setting預設的站點資料
-postgres_scripts.edit_the_default_platform()
-# 這邊要進行plt-basic的sync
-xxl.sync_plt_basics_data()
-wait_for_sync = 5
-time.sleep(wait_for_sync)
-
-# #############
-#  API 預處理  #
-# #############
-
-platform.super_admin_initialize()
-platform.create_dept_and_role()
-platform.create_account()
-platform.login_account()
-platform.turn_on_the_game_to_test()
-client_side.create_initial_user()
-client_side.binding_mobile()
-
-# #############
-#   postgres  #
-# #############
-
-postgres_scripts.host_platform()
-postgres_scripts.plt_account()
+# xxl = XxlJobs()
+# # ####################
+# #  postgres-總控模擬  #
+# # ####################
+# # setting預設的站點資料
+# postgres_scripts.edit_the_default_platform()
+# # 這邊要進行plt-basic的sync
+# xxl.sync_plt_basics_data()
+# wait_for_sync = 5
+# time.sleep(wait_for_sync)
+#
+# # #############
+# #  API 預處理  #
+# # #############
+#
+# platform.super_admin_initialize()
+# platform.create_dept_and_role()
+# platform.create_account()
+# platform.login_account()
+# platform.turn_on_the_game_to_test()
+# client_side.create_initial_user()
+# client_side.binding_mobile()
+#
+# # #############
+# #   postgres  #
+# # #############
+#
+# postgres_scripts.host_platform()
+# postgres_scripts.plt_account()
 postgres_scripts.plt_game()
-postgres_scripts.wallet()
-platform.sync_relation_manage()
-xxl.get_game_orders()
-
-# #############
-#    mongo    #
-# #############
-
-mongo_scripts.setup_lock_status_user()
-
-# #############
-#  elasticsearch  #
-# #############
-# elasticsearch_scripts.plt_action_log()
+# postgres_scripts.wallet()
+# platform.sync_relation_manage()
+# xxl.get_game_orders()
+postgres_scripts.plt_user()
+postgres_scripts.cs_user()
+# # #############
+# #    mongo    #
+# # #############
+#
+# mongo_scripts.setup_lock_status_user()
+#
+# # #############
+# #  elasticsearch  #
+# # #############
+# # elasticsearch_scripts.plt_action_log()
