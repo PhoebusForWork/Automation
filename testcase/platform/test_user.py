0ae2373c0139a2659d64102e012e425f8bf96dc1
PhoebusLiu <phoebusliu_gram@gmail.com>
2023-11-28 17:41:19 +0800

    第一版調整還沒整理完成

--- testcase/platform/test_user.py
+++ testcase/platform/test_user.py
@@ -2,7 +2,7 @@ import pytest
 import allure
 import jsonpath
 from pylib.platform.user import UserVip, User, UserManage , UserVipPointRatio
-from pylib.platform.proxy import ProxyManage
+from pylib.platform.proxy import ProxyManage, Proxy
 from utils.data_utils import TestDataReader, ResponseVerification,GetClassData
 from utils.api_utils import API_Controller
 from utils.json_verification import validate_json
@@ -308,9 +308,15 @@ class TestUser:
     @pytest.mark.parametrize("test", test_data.get_case('edit_user_convertProxy'))
     def test_edit_user_convertProxy(test, get_platform_token):
         if test['json']['userId'] == 'client_id':
-            user = User()
-            user_id = user.get_client_user(plat_token=get_platform_token)
+            user = User(token=get_platform_token)
+            user_id = user.get_client_user()
             test['json']['userId'] = user_id
+        elif test['json']['userId'] == 'proxy_id':
+            user = Proxy(token=get_platform_token)
+            user_id = user.get_proxy(queryType=0, input='prxoy001')
+            user_id = jsonpath.jsonpath(user_id, "$.data.[0].userId")[0]
+            test['json']['userId'] = user_id
+
         api = API_Controller()
         resp = api.send_request(test['req_method'], test['req_url'], test['json'],
                                 test['params'], token=get_platform_token)
